64a78f3c5e548af3fc120b06eb726f44b6619c42
Tyler Long <tyler4long@gmail.com>
2023-08-29 15:01:41 -0700

    sip provision

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello world!');
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  await rc.authorize({
+    jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
+  });
+  const sipProvision = await rc
+    .restapi()
+    .clientInfo()
+    .sipProvision()
+    .post({
+      sipInfo: [{ transport: 'WSS' }],
+    });
+  console.log(JSON.stringify(sipProvision, null, 2));
+  await rc.revoke();
+};
+
+main();
